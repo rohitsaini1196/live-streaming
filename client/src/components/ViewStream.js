c064667234185df87dd1a4f0723375f4dd29a536
Rohit Saini <sidsaini1196@gmail.com>
2021-05-27 18:37:05 +0530

    stream view video layout added

--- client/src/components/ViewStream.js
+++ client/src/components/ViewStream.js
@@ -1,9 +1,31 @@
+import { Paper } from "@material-ui/core";
 import React from "react";
 
 function ViewStream() {
   return (
     <div>
-      <p>Viewing Stream</p>
+      <div>
+        <div>
+          <video
+            width="100%"
+            height="auto"
+            controls
+            style={{ borderRadius: 5 }}
+          >
+            <source
+              src="http://techslides.com/demos/sample-videos/small.mp4"
+              type="video/mp4"
+            />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      </div>
+
+      <div>
+        <Paper>
+          <p>User Details</p>
+        </Paper>
+      </div>
     </div>
   );
 }
